bd5f6c2a9e5b4308324e3778230df4758a8e2005
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-08 16:41:35 +0700

    Implement checkRightMove to identify move is correct or incorrect

--- config/helpers.js
+++ config/helpers.js
@@ -1,13 +1,16 @@
-function isRightMove(move, characters, range) {
-  const result = characters.filter((character) => {
-    move.position_x > character.position_x + range &&
+function checkRightMove(move, characters, range) {
+  const result = characters.filter(
+    (character) =>
+      move.position_x > character.position_x - range &&
       move.position_x < character.position_x + range &&
-      move.position_y > character.position_y + range &&
+      move.position_y > character.position_y - range &&
       move.position_y < character.position_y + range &&
-      move.characterId == character.id;
-  });
+      move.characterId == character.id
+  );
 
   if (result.length > 0) {
-    return true;
-  } else return false;
+    return result[0];
+  } else return null;
 }
+
+module.exports = checkRightMove;
